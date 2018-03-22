e871e869bf080c138b5b2163d713461715ce2449
lucas2502 <lucas2502@live.com>
2018-03-22 18:03:41 -0300

    Update

--- coutdown.js
+++ coutdown.js
@@ -1,16 +1,16 @@
 
-var YY = 2018; // Ano
-var MM = 07; // Mês
-var DD = 22; // Dia
+var YY = 2018; //Ano
+var MM = 07; //Mês
+var DD = 22; //Dia
 var HH = 00; //Horas
-var MI = 00; // Minutos
-var SS = 00;  //Segundos
+var MI = 00; //Minutos
+var SS = 00; //Segundos
 
 var coutdownRefresh = function() {
-  var now = new Date();
-  var future = new Date(YY,MM-1,DD,HH,MI,SS); // Também recebe string ('000/00/00 00:00:00') 
+  var now = new Date(); 
+  var future = new Date(YY,MM-1,DD,HH,MI,SS); // new Date('0000/00/00 00:00:00) Também funciona 
 
-  var ss = parseInt((futuro - hoje) / 1000);
+  var ss = parseInt((future - now) / 1000);
   var mm = parseInt(ss / 60);
   var hh = parseInt(mm / 60);
   var dd = parseInt(hh / 24); 
@@ -20,21 +20,30 @@ var coutdownRefresh = function() {
   hh = hh - (dd * 24); 
 
   var missing = '';
-  faltam += (dd && dd > 1) ? dd+' dias, ' : (dd==1 ? '1 dia, ' : '');
-  faltam += (toString(hh).length) ? hh+' hr, ' : '';
-  faltam += (toString(mm).length) ? mm+' min e ' : '';
-  faltam += ss+' seg'; 
+  missing += (dd && dd > 1) ? dd + ' dias, ' : (dd == 1 ? '1 dia, ' : '');
+  missing += (toString(hh).length) ? hh + ' hr, ' : '';
+  missing += (toString(mm).length) ? mm + ' min e ' : '';
+  missing += ss + ' seg'; 
 
-  if (dd+hh+mm+ss > 0) {
-    console.log(missing);//Retorno da Contagem regressiva se maior que zero
+  if (dd + hh + mm + ss > 0) {
+    console.log(missing);
     setTimeout(coutdownRefresh,1000);
   } else {
-    console.log('FINISH!'); // Se não acabou a contagem regressiva
+    console.log('FINISH');
     setTimeout(coutdownRefresh,1000);
   }
-  
+ }
  
-}
-	
-	
-	
+ coutdownRefresh()
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
