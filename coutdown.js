ed0e49f9701bea069ea8b96fee7105b14e072e3f
lucas2502 <lucas2502@live.com>
2018-03-22 17:08:22 -0300

    Add Comentario

--- coutdown.js
+++ coutdown.js
@@ -1,14 +1,14 @@
 
-var YY = 2018;
-var MM = 07;
-var DD = 22;
-var HH = 00;
-var MI = 00;
-var SS = 00; 
+var YY = 2018; // Ano
+var MM = 07; // Mês
+var DD = 22; // Dia
+var HH = 00; //Horas
+var MI = 00; // Minutos
+var SS = 00;  //Segundos
 
 var coutdownRefresh = function() {
   var now = new Date();
-  var future = new Date(YY,MM-1,DD,HH,MI,SS); 
+  var future = new Date(YY,MM-1,DD,HH,MI,SS); // Também recebe string ('000/00/00 00:00:00') 
 
   var ss = parseInt((futuro - hoje) / 1000);
   var mm = parseInt(ss / 60);
@@ -26,10 +26,10 @@ var coutdownRefresh = function() {
   faltam += ss+' seg'; 
 
   if (dd+hh+mm+ss > 0) {
-    console.log(missing);
+    console.log(missing);//Retorno da Contagem regressiva se maior que zero
     setTimeout(coutdownRefresh,1000);
   } else {
-    console.log('CHEGOU!!!!');
+    console.log('FINISH!'); // Se não acabou a contagem regressiva
     setTimeout(coutdownRefresh,1000);
   }
   
